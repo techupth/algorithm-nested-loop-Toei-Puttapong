02b141ac32d28e46e8774562760fb081709e7cc1
Puttapong <puttapong.lekk@gmail.com>
2024-07-03 14:02:06 +0700

    feat:add ex1-ex3

--- ex-1.js
+++ ex-1.js
@@ -1,4 +1,15 @@
-// เริ่มเขียนโค้ดตรงนี้ 
-function printPattern() {
-   
+// เริ่มเขียนโค้ดตรงนี้
+function printPattern(n) {
+  let finalResult = "";
+  for (let i = 0; i <= n; i++) {
+    let result = "";
+    for (let j = 1; j <= i; j++) {
+      result += j + "\t";
+    }
+    finalResult += result + "\n";
+  }
+  return finalResult;
 }
+
+let input1 = printPattern(5);
+console.log(input1);
--- ex-2.js
+++ ex-2.js
@@ -1,4 +1,15 @@
-// เริ่มเขียนโค้ดตรงนี้ 
-function printStar() {
-   
+// เริ่มเขียนโค้ดตรงนี้
+function printStar(n) {
+  let finalResult = "";
+  for (let i = 1; i <= n; i++) {
+    let result = "";
+    for (let j = 1; j <= i; j++) {
+      result += "*" + "\t";
+    }
+    finalResult += result + "\n";
+  }
+  return finalResult;
 }
+
+let input1 = printStar(5);
+console.log(input1);
